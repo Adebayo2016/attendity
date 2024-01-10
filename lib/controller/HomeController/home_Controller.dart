e488af4a14cc5400bb89f42393a9c64b50db36b9
Walexhino <adewaleadebayo630@gmail.com>
2024-01-10 03:05:51 +0100

    updated attendance

---
+++ lib/controller/HomeController/home_Controller.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  // showMarkAsDoneDialog() {
+  //   Get.defaultDialog(
+  //     title: 'Mark as done',
+  //     content: Text('Are you sure you want to mark this task as done?'),
+  //     textConfirm: 'Yes',
+  //     textCancel: 'No',
+  //     confirmTextColor: Colors.white,
+  //     onConfirm: () {
+  //       Get.back();
+  //     },
+  //     onCancel: () {
+  //       Get.back();
+  //     },
+  //   );
+  // }
+
+      ShowMarkDialog (BuildContext context) {
+        AlertDialog(
+          title: Text("Mark as done"),
+          content: Text("Are you sure you want to mark  this Attendance ?"),
+          actions: [
+            TextButton(
+              child: Text("Yes"),
+              onPressed: () {
+                Get.back();
+              },
+            ),
+            TextButton(
+              child: Text("No"),
+              onPressed: () {
+                Get.back();
+              },
+            ),
+          ],
+        );
+      }
+
+     SelectMarkOption(){
+        Get.defaultDialog(
+          title: 'Select Mode',
+          content: Text('Are you sure you want to mark this task as done?'),
+          textConfirm: 'Location',
+          textCancel: 'QrCode',
+          confirmTextColor: Colors.white,
+          onConfirm: () {
+            Get.back();
+          },
+          onCancel: () {
+            Get.back();
+          },
+        );
+     }
+
+      }
