74978ae3bffa9bae7ae660c00aafa68949a40681
Walexhino <adewaleadebayo630@gmail.com>
2024-01-10 03:05:46 +0100

    updated attendance

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,12 @@
+import 'package:attendity/views/today.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_initicon/flutter_initicon.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../controller/HomeController/home_Controller.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,9 +16,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+
+      ),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.0.sp, vertical: 33.sp),
@@ -28,7 +39,9 @@ class _HomePageState extends State<HomePage> {
                       semanticsLabel: 'Attendity Logo',
                       height: 27,
                     ),
-                    SizedBox(width: 60.sp,),
+                    SizedBox(
+                      width: 60.sp,
+                    ),
                     SvgPicture.asset(
                       'assets/images/aten2.svg',
                       semanticsLabel: 'Attendity Logo',
@@ -37,22 +50,24 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
               ),
-              SizedBox(height: 40.sp,),
+              SizedBox(
+                height: 40.sp,
+              ),
               Container(
                 child: Row(
                   children: [
                     Container(
                       height: 60.sp,
                       width: 60.sp,
-                      child: CircleAvatar(
+                      child: Initicon(
                         //radius: 40.sp,
-                       child: SvgPicture.asset(
-                         'assets/images/hIcon.svg',
-                         semanticsLabel: 'Attendity Logo',
-                       ),
+                        text: 'Adebayo Adewale',
+
                       ),
                     ),
-                    SizedBox(width: 10.sp,),
+                    SizedBox(
+                      width: 10.sp,
+                    ),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
@@ -69,7 +84,7 @@ class _HomePageState extends State<HomePage> {
                           textAlign: TextAlign.left,
                           style: TextStyle(
                             fontSize: 12.sp,
-                           // fontWeight: FontWeight.w700,
+                            // fontWeight: FontWeight.w700,
                           ),
                         ),
                       ],
@@ -77,20 +92,196 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
               ),
-              SizedBox(height: 60.sp,),
-              Text("Hi, Adebayo",
+              SizedBox(
+                height: 60.sp,
+              ),
+              Text(
+                "Hi, Adebayo",
                 style: TextStyle(
                   fontSize: 30.sp,
                   fontWeight: FontWeight.w700,
                   color: Color(0xFF1A1A1A),
                 ),
               ),
-              SizedBox(height: 10.sp,),
-              Text("Welcome to your class",
+              SizedBox(
+                height: 10.sp,
+              ),
+              Text(
+                "Welcome to your class",
                 style: TextStyle(
                   fontSize: 30.sp,
                   fontWeight: FontWeight.w700,
                   color: Color(0xFF97BCE8),
+                ),
+              ),
+              SizedBox(
+                height: 50.sp,
+              ),
+              InkWell(
+                onTap: () {
+                  Get.to(() => Today());
+                },
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Today's Classes",
+                          style: TextStyle(
+                            fontSize: 24.sp,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xFF1A1A1A),
+                          )
+                      ),
+                      SvgPicture.asset('assets/images/Polygon.svg')
+                    ],
+
+                  ),
+                ),
+              ),
+              SizedBox(height:24.sp),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Color(0xFFF2F2F2),
+                  border: Border.all(
+                    color: Color(0xFF97BCE8),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 5.sp,),
+                    Row(
+                      children: [
+                        Initicon(text: 'IFT'),
+
+                        SizedBox(width: 20.sp,),
+                        Text("IFT 508",
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF1A1A1A),
+                            )
+                        ),
+                        SizedBox(width: 20.sp,),
+                        Text("8:00am - 10:00am",
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF1A1A1A),
+                            )
+                        ),
+                        SizedBox(width: 20.sp,),
+
+                      ],
+
+                    ),
+                    SizedBox(height: 5.sp,),
+                    Row(
+                      children: [
+                        Initicon(
+                          text: 'IFT',
+                        ),
+                        SizedBox(width: 20.sp,),
+                        Text("IFT 506",
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF1A1A1A),
+                            )
+                        ),
+                        SizedBox(width: 20.sp,),
+                        Text("10:00am - 12:00PM",
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF1A1A1A),
+                            )
+                        ),
+                        SizedBox(width: 20.sp,),
+
+                      ],
+
+                    ),
+                    SizedBox(height: 5.sp,),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          //radius: 40.sp,
+                            child: Text(
+                                'A'
+                            )
+                        ),
+                        SizedBox(width: 20.sp,),
+                        Text("CSC 508",
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF1A1A1A),
+                            )
+                        ),
+                        SizedBox(width: 20.sp,),
+                        Text("12:00pm -2:00pm",
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF1A1A1A),
+                            )
+                        ),
+                        SizedBox(width: 20.sp,),
+
+                      ],
+
+                    ),
+                    SizedBox(height: 5.sp,),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          //radius: 40.sp,
+                            child: Text(
+                                'A'
+                            )
+                        ),
+                        SizedBox(width: 20.sp,),
+                        Text("IFT 502",
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF1A1A1A),
+                            )
+                        ),
+                        SizedBox(width: 20.sp,),
+                        Text("8:00am - 10:00am",
+                            style: TextStyle(
+                              fontSize: 12.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Color(0xFF1A1A1A),
+                            )
+                        ),
+                        SizedBox(width: 20.sp,),
+
+                      ],
+
+                    )
+
+                  ],
+                ),
+              ),
+              SizedBox(height: 24.sp,),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Attendance History",
+                        style: TextStyle(
+                          fontSize: 24.sp,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xFF1A1A1A),
+                        )
+                    ),
+                    SvgPicture.asset('assets/images/Polygon.svg')
+                  ],
+
                 ),
               ),
 
